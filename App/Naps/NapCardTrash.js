f7ed93ee40163d4fb96f93ffe414ad46ce89cac3
Thiago Correa <thiagobocorrea@gmail.com>
2015-10-23 14:18:10 -0200

    delete working

--- App/Naps/NapCardTrash.js
+++ App/Naps/NapCardTrash.js
@@ -1,10 +1,12 @@
 import React from 'react-native';
+import moment from 'moment';
 import Icon from '../../node_modules/react-native-vector-icons/Ionicons';
 
 const {
     Component,
     StyleSheet,
     View,
+    ActionSheetIOS,
     } = React;
 
 import Text from './../components/Text'
@@ -23,8 +25,8 @@ export default class NapCardTrash extends Component {
   render() {
     return (
         <View style={[styles.container, {width: this.props.width}]}>
-          <View style={{marginRight: 30, marginBotton: 20}}>
-            <ButtonIcon underlayColor="transparent">
+          <View style={{marginRight: 30, marginBottom: 10}}>
+            <ButtonIcon underlayColor="transparent" onPress={this.showActionSheet.bind(this)}>
               <Icon name="ios-trash-outline" size={30} color="rgba(217, 217, 217, 0.55)" />
             </ButtonIcon>
           </View>
@@ -33,10 +35,48 @@ export default class NapCardTrash extends Component {
     )
   }
 
+  showActionSheet() {
+    let buttons = []
+    let intervals = this.getIntervals()
+    let totalItens = this.props.napSnapshot.child('intervals').numChildren()
+
+    intervals.forEach(interval => {
+      buttons.push(this.formatDate(interval.val().start) + ' - ' + this.formatDate(interval.val().end))
+    })
+
+    buttons.push('apaga tudo')
+    buttons.push('cancelar')
+
+    ActionSheetIOS.showActionSheetWithOptions({
+          options: buttons,
+          cancelButtonIndex: totalItens + 1,
+          destructiveButtonIndex: totalItens,
+        },
+        (buttonIndex) => {
+          if (buttonIndex == totalItens) {
+            this.props.napSnapshot.ref().remove()
+          } else if (buttonIndex < totalItens) {
+            intervals[buttonIndex].ref().remove()
+          }
+        });
+  }
+
+  getIntervals() {
+    let result = []
+    this.props.napSnapshot.child('intervals').forEach(interval => {
+      console.log(interval.key())
+      result.push(interval)
+    })
+    return result
+  }
+
+  formatDate(date) {
+    if (!date) {return ''}
+    return moment.unix(date).format('HH:mm')
+  }
 }
 
 NapCardTrash.propTypes = {
   napSnapshot: React.PropTypes.object.isRequired,
-  idx: React.PropTypes.number.isRequired,
   width: React.PropTypes.number.isRequired
 };
